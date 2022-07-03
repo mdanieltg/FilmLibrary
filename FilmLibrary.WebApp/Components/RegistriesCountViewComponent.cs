7647e50dc3e025d2f439be71776a280e8f5fd160
Daniel Medrano Huerta <daniel@quicktortoise.com>
2022-07-03 13:09:11 -0500

    Agregar componente de vista RegistriesCount

---
+++ FilmLibrary.WebApp/Components/RegistriesCountViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using FilmLibrary.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmLibrary.WebApp.Components;
+
+public class RegistriesCountViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(int itemCount, int totalItems)
+    {
+        return View(new RegistriesCountModel { ItemCount = itemCount, TotalItems = totalItems });
+    }
+}
---
+++ FilmLibrary.WebApp/Models/RegistriesCountModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FilmLibrary.WebApp.Models;
+
+public class RegistriesCountModel
+{
+    public int ItemCount { get; set; }
+    public int TotalItems { get; set; }
+}
