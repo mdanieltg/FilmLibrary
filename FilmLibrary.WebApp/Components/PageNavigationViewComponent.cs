85729967cae8f3e7ab19e0a5ba0bc105004cdb48
Daniel Medrano Huerta <daniel@quicktortoise.com>
2022-07-02 21:03:11 -0500

    Crear componente de vista PageNavigation
    Agregar modelo PageNavigationModel

---
+++ FilmLibrary.WebApp/Components/PageNavigationViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using FilmLibrary.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmLibrary.WebApp.Components;
+
+public class PageNavigationViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(int currentPage, int pageSize, int totalPages)
+    {
+        var pageNavigationModel = new PageNavigationModel
+        {
+            CurrentPage = currentPage,
+            PageSize = pageSize,
+            TotalPages = totalPages
+        };
+        return View(pageNavigationModel);
+    }
+}
---
+++ FilmLibrary.WebApp/Models/PageNavigationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmLibrary.WebApp.Models;
+
+public class PageNavigationModel
+{
+    public int CurrentPage { get; set; }
+    public int? PreviousPage => CurrentPage <= 1 ? null : CurrentPage - 1;
+    public int? NextPage => CurrentPage >= TotalPages ? null : CurrentPage + 1;
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+}
