6fa09f4ef5fe945e8c7bf334c66e3af378d40a33
Daniel Medrano Huerta <daniel@quicktortoise.com>
2022-07-02 22:08:39 -0500

    Crear componente de vista PageInformation
    Agregar modelo PageInformationModel

---
+++ FilmLibrary.WebApp/Components/PageInformationViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using FilmLibrary.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmLibrary.WebApp.Components;
+
+public class PageInformationViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(int currentPage, int totalPages)
+    {
+        return View(new PageInformationModel { CurrentPage = currentPage, TotalPages = totalPages });
+    }
+}
---
+++ FilmLibrary.WebApp/Models/PageInformationModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FilmLibrary.WebApp.Models;
+
+public class PageInformationModel
+{
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+}
